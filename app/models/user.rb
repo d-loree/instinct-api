4c33f78e0134827796c6344dc763ac0db913fae2
Dylan Loree <dylan.loree@gmail.com>
2024-06-01 13:30:55 -0400

    Added jti and rack session fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable.
+         :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
 end
