56b89bbddf852def96f0176bb5091cfa4a1bf347
Dylan Loree <dylan.loree@gmail.com>
2024-06-25 23:17:21 -0400

    linted code with rubocop

--- db/migrate/20240612012008_add_privacy_to_playlists.rb
+++ db/migrate/20240612012008_add_privacy_to_playlists.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddPrivacyToPlaylists < ActiveRecord::Migration[7.1]
   def change
     add_column :playlists, :privacy, :string, null: false, default: 'public'
