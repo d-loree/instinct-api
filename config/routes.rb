352282dab7bd13528881ab8cc420ea00f3fa103f
Will <william@wisaksana.com>
2024-10-19 19:26:57 -0400

    Add function for getting all public songs and route

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,11 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :songs
+  resources :songs do
+    collection do 
+      get "public_songs", to: "songs#public_songs" 
+    end
+  end
 
   get 'up' => 'rails/health#show', as: :rails_health_check
 end
